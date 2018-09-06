9995515af6cadffc9f4a8ce8945dddd789e7ae08
JohnAndre2015 <johnandre1019@gmail.com>
2018-09-06 18:26:41 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/iicsust/raymundo/johnandre/lab2/Raymundo_Lab2.java
@@ -0,0 +1,13 @@
+package ph.edu.iicsust.raymundo.johnandre.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Raymundo_Lab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raymundo__lab2);
+    }
+}
